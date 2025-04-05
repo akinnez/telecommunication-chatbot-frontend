5468956c7969aece4f0febcdd77262c3050fb3df
akinnez <oyetunjiidris@gmail.com>
2025-04-05 09:30:46 +0100

    implement reload function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ type Message = {
 export default function Home() {
 	const [messages, setMessages] = useState<Message[]>([]);
 	const [input, setInput] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
 	const chatEndRef = useRef<HTMLDivElement | null>(null);
 
 	const scrollToBottom = () => {
@@ -25,19 +26,31 @@ export default function Home() {
 		mutationFn: chat,
 	});
 
-	const handleSend = async () => {
-		if (!input.trim()) return;
+	const handleSend = async (customInput?: string) => {
+		setErrorMessage('');
+		const messageToSend = customInput ?? input;
+		if (!messageToSend.trim()) return;
 
-		const userMessage: Message = {sender: 'user', text: input};
-		setMessages((prev) => [...prev, userMessage]);
+		const userMessage: Message = {sender: 'user', text: messageToSend};
+		setMessages((prev) => {
+			const hasMatch = messages.some(
+				(obj: Message) => obj.text === userMessage.text
+			);
+			if (hasMatch) {
+				return [...prev];
+			}
+			return [...prev, userMessage];
+		});
 		setInput('');
 
 		mutate(input, {
-			onError(error) {
-				setMessages((prev) => [
-					...prev,
-					{sender: 'bot', text: error.message},
-				]);
+			// @typescript-eslint/no-explicit-any
+			onError(error: any) {
+				const message =
+					error?.response?.data?.message ||
+					error.message ||
+					'Something went wrong';
+				setErrorMessage(message);
 			},
 			// @typescript-eslint/no-explicit-any
 			onSuccess(data: any) {
@@ -56,19 +69,64 @@ export default function Home() {
 			<div className="lg:px-5 lg:grid lg:grid-cols-4 gap-x-10">
 				<div className="rounded-br-4xl rounded-tl-4xl m-5 col-span-1 min-h-[calc(100vh-3rem)] bg-foreground dark:bg-card hidden lg:block"></div>
 				<div className="lg:col-span-3 bg-card relative lg:rounded-4xl min-h-[calc(100vh-8rem)] lg:min-h-[calc(100vh-3rem)] w-full">
-					<div className="flex-1 overflow-y-auto p-4 space-y-5 max-h-[calc(100vh-8rem)] lg:max-h-[calc(100vh-6rem)]">
-						{messages.map((msg, i) => (
-							<div
-								key={i}
-								className={`px-3 lg:max-w-md max-w-[calc(100%-3rem)]  ${
-									msg.sender === 'user'
-										? 'text-foreground self-end ml-auto'
-										: 'text-card self-start mr-auto'
-								}`}
-							>
-								{isPending ? 'Loading ...' : msg.text}
+					<div className="flex-1 overflow-y-auto p-4 space-y-3 max-h-[calc(100vh-5.8rem)]  h-[calc(100svh-13.59rem)] lg:h-fit">
+						{messages.length == 0 ? (
+							<div className="w-full flex min-h-[calc(100vh-8rem)] items-center justify-center">
+								<div className="space-y-4">
+									<h1 className="text-xl lg:text-3xl">
+										Hello there😎❤️. I'm KIRA.
+									</h1>
+									<p className="text-center text-xl">
+										How can i assist you today?
+									</p>
+								</div>
 							</div>
-						))}
+						) : (
+							messages.map((msg, i) => (
+								<div key={i}>
+									{msg.sender === 'user' && (
+										<>
+											<div
+												key={i}
+												className={`py-4 px-3 max-w-fit lg:max-w-lg rounded-lg bg-blue-950 text-foreground self-end ml-auto
+									}`}
+											>
+												{msg.text}
+											</div>
+											{errorMessage && (
+												<div className="flex gap-x-5 max-w-fit lg:max-w-lg items-center mt-2 text-sm text-foreground self-end ml-auto">
+													<p>{errorMessage}</p>
+													<button
+														onClick={async () => {
+															setInput(msg.text);
+															handleSend(
+																msg.text
+															);
+														}}
+														className="underline cursor-pointer text-blue-600"
+													>
+														Reload
+													</button>
+												</div>
+											)}
+										</>
+									)}
+									{msg.sender === 'bot' && !errorMessage && (
+										<div
+											key={i}
+											className={`py-4 px-3 max-w-fit lg:max-w-lg rounded-lg ${'bg-foreground/85 text-card self-start mr-auto'}`}
+										>
+											{msg.text}
+										</div>
+									)}
+								</div>
+							))
+						)}
+						{isPending && (
+							<div className="py-4 px-3 max-w-fit lg:max-w-lg rounded-lg bg-foreground/60 text-card self-start mr-auto animate-pulse">
+								KIRA is typing...
+							</div>
+						)}
 					</div>
 
 					<div className="absolute bottom-0 w-full px-10 py-5">
@@ -88,7 +146,7 @@ export default function Home() {
 								<button
 									disabled={isPending}
 									className="bg-foreground text-card px-6 py-3 rounded-full"
-									onClick={handleSend}
+									onClick={() => handleSend()}
 								>
 									Send
 								</button>
